9652d5c05bfa573030966d180ba9bdb67f790139
carlopizzuto <carlo.pizzutow@gmail.com>
2025-04-17 16:57:35 -0500

    fix/plugins.lsp: removed unnecesary warnings for lua_ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,33 +5,48 @@ return {
 	{ "williamboman/mason.nvim", build = ":MasonUpdate" },
   	
 	{ 
-	"williamboman/mason-lspconfig.nvim",
-    	opts = {
-		ensure_installed = { "lua_ls", "pyright", "rust_analyzer" },
-		automatic_installation = true,
-	},
-	config = function(_, opts)
+		"williamboman/mason-lspconfig.nvim",
+    		opts = {
+			ensure_installed = { "lua_ls", "pyright", "rust_analyzer" },
+			automatic_installation = true,
+		},
+		config = function(_, opts)
 			require("mason").setup()
 			require("mason-lspconfig").setup(opts)
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
+
+			lspconfig.lua_ls.setup({
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+							disable = {},
+						},
+						telemetry = {
+							false
+						},
+					},
+				},
+			})
+
 			lspconfig.pyright.setup({})
-      			lspconfig.rust_analyzer.setup({})
-    	end,
+
+      		lspconfig.rust_analyzer.setup({})
+    		end,
   	},
   	
 	{ 
-	"hrsh7th/nvim-cmp",
-	dependencies = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip" },
-	config = function()
-      		local cmp = require("cmp")
-		cmp.setup({
-			sources = { { name = "nvim_lsp" } },
-			mapping = cmp.mapping.preset.insert({
-				["<Tab>"] = cmp.mapping.confirm({ select = true }),
-        		}),
+		"hrsh7th/nvim-cmp",
+		dependencies = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip" },
+		config = function()
+      			local cmp = require("cmp")
+			cmp.setup({
+				sources = { { name = "nvim_lsp" } },
+				mapping = cmp.mapping.preset.insert({
+					["<Tab>"] = cmp.mapping.confirm({ select = true }),
+        			}),
       		})
-    	end,
+    		end,
 	},
 }
 
