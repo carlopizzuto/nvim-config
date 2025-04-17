d8723df5648429c9aa035a8c9c7e1bc16b1f9bbf
carlopizzuto <carlo.pizzutow@gmail.com>
2025-04-17 16:55:40 -0500

    dev/config.options: added commments

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,10 +4,14 @@ local opt = vim.opt
 vim.g.mapleader		= " "
 vim.g.maplocalleader 	= "\\"
 
-opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
+-- sync with system clipboard
+opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" 
 
+-- enable line numbers
 opt.number = true
 
+-- make each shift 5 spaces long
 opt.shiftwidth = 5
+-- make each tab 5 spaces long
 opt.tabstop = 5
 
