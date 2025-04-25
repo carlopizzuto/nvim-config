6a846d4c378def842a63989567db2abd2c53742c
carlopizzuto <carlo.pizzutow@gmail.com>
2025-04-25 13:37:50 -0500

    feat/config.keymaps: shortcuts for Lazy and Telescope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,12 @@
 -- nvim keymaps
 
+-- open Lazy
+vim.keymap.set("n", "<leader>L", "<CMD>Lazy<CR>", { desc = "Open Lazy", noremap = true, silent = true })
+
+-- open Telescope
+vim.keymap.set("n", "<leader>T", "<CMD>Telescope<CR>", { desc = "Open Telescope", noremap = true, silent = true })
+
+
 -- open nvim-tree file explorer 
 vim.keymap.set("n", "<leader>e", "<CMD>NvimTreeToggle<CR>", { desc = "Toggle Nvim Tree File Explorer", noremap = true, silent = true })
 
